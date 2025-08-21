d7cdcc6be5eb8c8c739c27004b405ff5a4c6e1b4
Ben M <109357901+ben-me@users.noreply.github.com>
2025-08-21 12:03:18 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
+
+  modules: ["@nuxt/image", "@nuxt/test-utils", "@nuxt/ui", "@nuxt/eslint"],
+});
+
