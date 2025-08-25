c3aad63bd83db77758d11dc5a50a6e8bc9bbb0f2
Ben M <109357901+ben-me@users.noreply.github.com>
2025-08-25 13:00:15 +0200

    fix: Updated Header to fixed position along with mobile navigation bar

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -12,9 +12,9 @@ let mobile_nav_active = ref(false);
 
 <template>
   <header
-    class="flex justify-between items-center mt-4 mx-2.5 bg-[#8E8E8E]/25 px-4 py-2 rounded-md"
+    class="flex w-9/10 left-0 right-0 mx-auto z-10 fixed justify-between items-center mt-4 bg-[#8E8E8E]/25 px-4 py-2 rounded-md"
   >
-    <h1 class="text-white font-lobster">Demo Hair</h1>
+    <h1 class="text-white font-lobster text-2xl">Demo Hair</h1>
     <nav class="hidden md:inline-block font-semibold">
       <ul class="flex gap-8">
         <li v-for="link of LINKS">
@@ -36,13 +36,13 @@ let mobile_nav_active = ref(false);
       @click="mobile_nav_active = true"
       aria-label="Open mobile navigation"
     >
-      <Icon name="quill:hamburger" class="size-10" />
+      <Icon name="quill:hamburger" class="size-10 text-white" />
     </button>
 
     <div
       v-if="mobile_nav_active"
       @click="mobile_nav_active = false"
-      class="fixed inset-0 h-full w-full bg-black/20 md:hidden"
+      class="fixed bg-black/20 md:hidden inset-0 w-full h-full"
     ></div>
 
     <Transition
@@ -55,7 +55,7 @@ let mobile_nav_active = ref(false);
     >
       <nav
         v-if="mobile_nav_active"
-        class="fixed z-10 right-0 top-0 w-9/10 h-full bg-secondary font-semibold text-3xl"
+        class="fixed bg-secondary h-full w-9/10 top-0 right-0 font-semibold text-3xl"
       >
         <ul class="flex flex-col gap-8 items-center w-full">
           <li class="self-end pt-6 px-8.5">
