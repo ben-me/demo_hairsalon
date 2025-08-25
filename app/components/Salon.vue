15e1223d8b014ceb35cebe40ac205df6f0079ee5
Ben M <109357901+ben-me@users.noreply.github.com>
2025-08-25 14:20:29 +0200

    fix: Fix salon part styling

--- app/components/Salon.vue
+++ app/components/Salon.vue
@@ -1,18 +1,26 @@
 <template>
   <section class="px-6 py-10 flex flex-col gap-5">
     <h2 class="font-nunito text-4xl">Unser Salon</h2>
-    <NuxtImg
-      src="img/salon.webp"
-      class="h-[390px] w-full object-cover rounded-md"
-    />
-    <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-      diam voluptua.
-    </p>
+    <div class="grid md:grid-cols-[35%_1fr] gap-5 md:gap-10">
+      <NuxtImg
+        src="img/salon.webp"
+        class="h-[390px] w-full object-cover rounded-md md:h-[400px]"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+        est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
   </section>
 </template>
