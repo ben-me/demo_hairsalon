89f553a88f1702b0e983e92a987475d72846a583
Ben M <109357901+ben-me@users.noreply.github.com>
2025-08-28 11:38:22 +0200

    fix: Move header over to the right and apply more border radius on the picture.

--- app/components/Salon.vue
+++ app/components/Salon.vue
@@ -1,26 +1,29 @@
 <template>
-  <section class="px-6 py-10 flex flex-col gap-5">
-    <h2 class="font-nunito text-4xl">Unser Salon</h2>
+  <section class="px-6 py-10 lg:py-15 lg:px-20 flex flex-col gap-5">
     <div class="grid md:grid-cols-[35%_1fr] gap-5 md:gap-10">
       <NuxtImg
         src="img/salon.webp"
-        class="h-[390px] w-full object-cover rounded-md md:h-[400px]"
+        class="h-[390px] w-full object-cover rounded-lg md:h-[400px]"
       />
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet.
-      </p>
+      <div class="grid gap-1">
+        <h2 class="font-nunito text-4xl">Unser Salon</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet.
+        </p>
+      </div>
     </div>
   </section>
 </template>
