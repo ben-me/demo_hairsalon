ad071f698665199e1ddbcbaeb5ce3722f00b34ef
Ben M <109357901+ben-me@users.noreply.github.com>
2025-09-09 10:39:55 +0200

    fix: Remove useSeoMeta to potentially fix deploy.

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <script>
 import "~/assets/css/main.css";
-
-useSeoMeta({
-  description:
-    "Willkommen auf meiner Portfoliowebseite eines fiktionalen Friseursalons der CodePilots. So könnte Ihre, von mir erstellte, Webseite aussehen.",
-});
 </script>
 <template>
   <NuxtRouteAnnouncer />
