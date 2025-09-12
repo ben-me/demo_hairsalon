c131ce9e6fb2d4fbb8424f3523b714d6e52e070a
Ben M <109357901+ben-me@users.noreply.github.com>
2025-09-12 10:45:47 +0200

    feat: Make price carousel adjust height automatically.

--- app/components/SalonPreise.vue
+++ app/components/SalonPreise.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import data from "~/assets/data/leistungen.json";
 import emblaCarouselVue from "embla-carousel-vue";
+import AutoHeight from "embla-carousel-auto-height";
 type haircut_group = "Damen" | "Herren" | "Kinder";
 
-const [emblaRef, emblaApi] = emblaCarouselVue();
+const [emblaRef, emblaApi] = emblaCarouselVue({}, [AutoHeight()]);
 const categories = Object.keys(data.leistungen) as haircut_group[];
 const selected_price_list = ref(0);
 
@@ -27,6 +28,7 @@ function selectPriceList(index: number) {
       <menu class="mx-auto flex w-full max-w-60 justify-between">
         <li v-for="(category, index) in categories" :key="category">
           <button
+            :aria-selected="selected_price_list === index"
             :class="[
               selected_price_list === index && 'underline underline-offset-2',
             ]"
@@ -38,11 +40,11 @@ function selectPriceList(index: number) {
       </menu>
     </div>
     <div ref="emblaRef" class="overflow-hidden">
-      <div class="flex gap-3">
+      <div class="flex items-start gap-3 transition-[height] duration-150">
         <ul
           v-for="(category, index) in data?.leistungen"
           :key="index"
-          class="bg-bg grid flex-[0_0_100%] grid-cols-1 content-start items-start gap-8 rounded-md px-8 py-10 text-black md:grid-cols-2"
+          class="bg-bg grid h-full flex-[0_0_100%] grid-cols-1 content-start items-start gap-8 rounded-md px-8 py-10 text-black md:grid-cols-2"
         >
           <li
             v-for="haircut in category"
